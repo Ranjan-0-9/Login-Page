e7805d6b9213f41bd348191325a12cfb33799b8a
Ranjan-0-9 <ashutoshranjan34309@gmail.com>
2024-12-22 11:07:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    
+    let ui = document.getElementById("username");
+    let ei = document.getElementById("email");
+    let pi = document.getElementById("password");
+    let lb = document.getElementById("btn2");
+
+
+    lb.addEventListener("click", () =>{
+       
+        let username = ui.value;
+        let email = ei.value;
+        let password = pi.value;
+
+        
+        console.log(`Username-> ${username}`);
+        console.log(`email-> ${email}`);
+        console.log(`password-> ${password}`);
+
+        
+    });
+});
