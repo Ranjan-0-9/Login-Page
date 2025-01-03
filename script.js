bcc1e9ab2a4bd526e36db75ce4d87425b2d7f372
Ranjan-0-9 <ashutoshranjan34309@gmail.com>
2025-01-03 17:36:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-
-let ui = document.getElementById("username");
-let ei = document.getElementById("email");
-let pi = document.getElementById("password");
-let lb = document.getElementById("btn2");
-
-lb.addEventListener("click", () =>{
+document.addEventListener("DOMContentLoaded", ()=> {
+    
+    let ui = document.getElementById("username");
+    let ei = document.getElementById("email");
+    let pi = document.getElementById("password");
+    let lb = document.getElementById("btn2");
+    lb.addEventListener("click", () =>{
        
-    let username = ui.value;
-    let email = ei.value;
-    let password = pi.value;
+        let username = ui.value;
+        let email = ei.value;
+        let password = pi.value;
         
-    console.log(`Username-> ${username}`);
-    console.log(`email-> ${email}`);
-    console.log(`password-> ${password}`);
-
+        console.log(`Username-> ${username}`);
+        console.log(`email-> ${email}`);
+        console.log(`password-> ${password}`);
         
+    });
 });
-
